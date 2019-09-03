9a2bbc95e989000c38306ad77720e4f6d87a4fcf
Ted Osuch <ted@osuch.com>
2019-09-03 16:14:55 +0100

    third commit

--- paintcan.js
+++ paintcan.js
@@ -2,12 +2,14 @@ const PaintCan = function(volume) {
   this.paintVolume = volume;
 };
 
-PaintCan.prototype.empty = function() {
+PaintCan.prototype.checkEmpty = function() {
   if (this.paintVolume === 0) {
     return true;
   } else {
     return false;
   }
 };
+
+
 
 module.exports = PaintCan;
--- specs/paint_specs.js
+++ specs/paint_specs.js
@@ -15,10 +15,13 @@ describe ('PaintCan', function() {
   })
 
   it('should be able to check if empty', function () {
-    const actual = testPaintCan.empty();
+    const actual = testPaintCan.checkEmpty();
     assert.strictEqual(actual, false);
   })
 
-  xit('should be able to empty paint')
+  xit('should be able to empty paint', function(){
+    const actual = testPaintCan.usePaint();
+    assert.strictEqual(actual, false)
+  })
 
 })
